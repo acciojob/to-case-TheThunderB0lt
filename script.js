b00e32b98843a4e38e23597e7f46925f967c20ab
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-15 21:21:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toCase(text) {
-  // write your code here
+    if (!text || text.length === 0) {
+        return '-';
+    }
+
+    const lowerCase = text.toLowerCase();
+    const upperCase = text.toUpperCase();
+
+    return lowerCase + '-' + upperCase;
 }
-
-// DO not change the code below
-
-const text = prompt("Enter text:");
-alert(toCase(text));
